d2593c4c62e78e63cda8d69dd59ebbd0e0b035a4
Matheus Luís <matheus.bernardi@lnls.br>
2023-11-27 10:59:11 -0300

    use swagger plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { api } from "./api";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .use(
+    swagger({
+      path: "/docs",
+      documentation: {
+        info: {
+          title: "Snail API Documentation",
+          version: "0.0.1",
+        },
+      },
+    })
+  )
+  .use(api)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
